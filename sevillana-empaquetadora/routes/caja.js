7fc70b1be3f6c4c3d18596f5134a1b1db40b237a
Daniel <silverhawkdeveloper@gmail.com>
2023-05-22 20:08:10 +0200

    rutas

--- sevillana-empaquetadora/routes/caja.js
+++ sevillana-empaquetadora/routes/caja.js
@@ -13,26 +13,26 @@ router.post('/', function (req, res, next) {
 });
 
 // DELETE - Eliminar una caja identificado por su _id
-router.delete('/delete', function (req, res, next) {
-  caja.findByIdAndDelete(req.body._id, function (error, cajaInfo) {
+router.delete('/delete/:id', function (req, res) {
+  caja.findByIdAndDelete({ '_id': req.params.id }, function (error) {
     if (error) res.status(500).send(error);
     else res.sendStatus(200);
   });
 });
 
 // PUT - Actualizar una caja identificado por su _id
-router.put('/update', function (req, res, next) {
-  caja.findByIdAndUpdate(req.body._id, req.body, function (error) {
+router.put('/update/:id', function (req, res) {
+  caja.findByIdAndUpdate({ '_id': req.params.id }, req.body, function (error) {
     if (error) res.status(500).send(error);
     else res.sendStatus(200);
   });
 });
 
-// GET - Listar una caja por su _id
-router.get('/mostrar', function (req, res, next) {
-  caja.findById(req.body._id, function (error, cajaInfo) {
-    if (error) res.status(500).send(error);
-    else res.status(200).json(cajaInfo);
+// GET - Listar un único productos por su _id
+router.get('/:id', function(req, res) {
+  caja.findById({ '_id': req.params.id }, function(error, cajaInfo) {
+      if (error) res.status(500).send(error);
+      else res.status(200).json(cajaInfo);;
   });
 });
 
